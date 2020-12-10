82bd4a36a2a3d60ffd5b3b62d3f1da83ddbced45
Abhishek-TS <abhishekt@edmingle>
2020-12-10 15:16:05 +0530

    Bottom Navigation Added

--- app/src/main/java/com/edmingle/materialtodo/MainActivity.java
+++ app/src/main/java/com/edmingle/materialtodo/MainActivity.java
@@ -1,14 +1,66 @@
 package com.edmingle.materialtodo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Tasks");
+        loadFragment(new TaskFragment());
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_gifts:
+                    toolbar.setTitle("Tasks");
+                    loadFragment(new TaskFragment());
+                    return true;
+                case R.id.navigation_cart:
+                    toolbar.setTitle("Cart");
+                    loadFragment(new ShopFragment());
+                    return true;
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Profile");
+                    loadFragment(new ProfileFragment());
+                    return true;
+            }
+            loadFragment(fragment);
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
